a47d9ef4e5b5fecf2bad4e6410138f724dad2051
ANASTASIOSBELLOS <cs05474@uoi.gr>
2024-10-20 19:49:10 +0300

    LAB2FINISHED

--- lab02.s
+++ lab02.s
@@ -12,6 +12,22 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+#Finding the last index forward
+finding_the_last_index_forward:
+add s0, zero, zero   #initialize the value of s0 to 0
+
+loop :
+    beq s0, a1, done  #if array is null or it is finished
+    lw t0, 0(a0)
+    bnq t0, a2 ,next_word
+    add s0, a0 ,zero
+
+next_word:
+    addi a0, a0, 4
+    addi a1, a1,-1
+    j loop
+
+
 #-----------------------------
 done:
     addi a7, zero, 10 
